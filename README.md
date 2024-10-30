eb13a1bf944ae826f1fae5d9acfdd3f8c563a4a0
Andrea de Martinis <andreademartinis86@gmail.com>
2024-10-30 16:35:32 +0100

    Update account tester and readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # The WIld Oasis Hotel React App
 
-Hotel app from the project "The wild oasis" by Jonas Schmedtmann Ultimate React Course
+Hotel app from the project "The wild oasis" by Jonas Schmedtmann Ultimate React Course.
+Built with React Query and Supabase
--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("tester@mail.it");
+  const [password, setPassword] = useState("passtest1234");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
